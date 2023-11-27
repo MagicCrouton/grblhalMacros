007ba5c2d5cf3fc948a14d1f9f8264b01e84d37a
eugene park <e.park5336@gmail.com>
2023-11-26 20:43:52 -0500

    adjusted the location according to personal Machine

--- OpenBuildsSemiAutoToolChange.js
+++ OpenBuildsSemiAutoToolChange.js
@@ -1,29 +1,31 @@
-function findCircleCenter(approxCircleDia, endmillDiameter, probeFeed) {
+    const fastPrbSpd = 500;
+    const slowPrbSpd = 100;
+    const maxTravel = 20;
+    const toolStrLocal = `G0 X-700 Y-700 Z-50` // Location of the tool setter, this must be at a safe height for any tool you might have.
+    const toolChngLocal = `G0 X-600 Y-600 Z-50` // Location that you will perform the manual tool change. It can be same location as the tool setter.
+    
+function semiAutoToolChange(fastPrbSpd,slowPrbSpd,maxTravel,toolStrLocal,toolChngLocal) {
 
-    // var approxCircleDia = 60;
-    // var endmillDiameter = 3.175
-    // var probeFeed = 200;
-  
     var step = 0;
-    var rightside = 0,
-      leftside = 0,
-      farside = 0,
-      nearside = 0,
-      centerXdistance = 0,
-      centerYdistance = 0;
+    var initialZOffset = 0;
+    socket.off('status');
+    socket.on(`status`, function(status){
+      console.log(status);
+      initialZOffset=status.machine.position.offset.z;
+      console.log(`i think the work offset is ${initialZOffset}`)
+    })
+    // socket.off('status');
   
-    socket.off('prbResult'); // Disable old listeners
-  
-    var holefindermacroStep1 = `
+    var step1 = `
     ; Header
   
-    G21 ; mm mode
-    G10 P1 L20 X0 Y0 Z0 ; zero out current location
-  
-    G38.2 X` + approxCircleDia / 2 + ` F` + probeFeed + ` ; Probe X` // find right side of circle
+    ${toolStrLocal} ; Example coordinates for the tool setter location
+
+    M0 ; Pause for manually lowering tool closer to the setter` // find right side of circle
   
     socket.emit('runJob', {
       data: holefindermacroStep1,
+
       isJob: false,
       completedMsg: false,
       fileName: ""
@@ -37,8 +39,6 @@ function findCircleCenter(approxCircleDia, endmillDiameter, probeFeed) {
         // Steps 1-3 just positions endmill for probes that count
   
         if (step == 1) {
-          rightside = prbdata.x
-          console.log(rightside);
   
           var holefindermacroStep5 = `
           G4 P0.3
@@ -123,50 +123,50 @@ function findCircleCenter(approxCircleDia, endmillDiameter, probeFeed) {
     })
   }
   
-  Metro.dialog.create({
-    title: "Center Finding Macro",
-    content: `
-      <div class="row mb-0">
-        <label class="cell-sm-6">Maximum Distance between edges</label>
-        <div class="cell-sm-6">
-            <input id="centerProbeDistance" type="number" value="100" data-role="input" data-append="mm" data-prepend="<i class='fas fa-ruler-combined'></i>" data-clear-button="false">
-        </div>
-      </div>
+  // Metro.dialog.create({
+  //   title: "Center Finding Macro",
+  //   content: `
+  //     <div class="row mb-0">
+  //       <label class="cell-sm-6">Maximum Distance between edges</label>
+  //       <div class="cell-sm-6">
+  //           <input id="centerProbeDistance" type="number" value="100" data-role="input" data-append="mm" data-prepend="<i class='fas fa-ruler-combined'></i>" data-clear-button="false">
+  //       </div>
+  //     </div>
   
-      <small>This is the approximate diameter of the circle, or the maximum width between edges of the rectangular/square hole you are probing inside</small>
-      <hr>
-      <div class="row mb-0">
-        <label class="cell-sm-6">Endmill Diameter</label>
-        <div class="cell-sm-6">
-            <input id="centerProbeEndmill" type="number" value="6.35" data-role="input" data-append="mm" data-prepend="<i class='fas fa-arrows-alt-h'></i>" data-clear-button="false">
-        </div>
-      </div>
-      <small>Enter the Endmill Diameter</small>
-      <hr>
-      <div class="row mb-0">
-        <label class="cell-sm-6">Probe Feedrate</label>
-        <div class="cell-sm-6">
-            <input id="centerProbeFeedrate" type="number" value="100" data-role="input" data-append="mm/min" data-prepend="<i class='fas fa-running'></i>" data-clear-button="false">
-        </div>
-      </div>
-      <small>How fast the probe will move - slower is safer/more accurate</small>
-      `,
-    actions: [{
-        caption: "Run center finding Probe",
-        cls: "js-dialog-close success",
-        onclick: function() {
-          var approxCircleDia = parseFloat($("#centerProbeDistance").val())
-          var endmillDiameter = parseFloat($("#centerProbeEndmill").val())
-          var probeFeed = parseInt($("#centerProbeFeedrate").val())
-          findCircleCenter(approxCircleDia, endmillDiameter, probeFeed)
-        }
-      },
-      {
-        caption: "Cancel",
-        cls: "js-dialog-close alert",
-        onclick: function() {
-          //
-        }
-      }
-    ]
-  });
+  //     <small>This is the approximate diameter of the circle, or the maximum width between edges of the rectangular/square hole you are probing inside</small>
+  //     <hr>
+  //     <div class="row mb-0">
+  //       <label class="cell-sm-6">Endmill Diameter</label>
+  //       <div class="cell-sm-6">
+  //           <input id="centerProbeEndmill" type="number" value="6.35" data-role="input" data-append="mm" data-prepend="<i class='fas fa-arrows-alt-h'></i>" data-clear-button="false">
+  //       </div>
+  //     </div>
+  //     <small>Enter the Endmill Diameter</small>
+  //     <hr>
+  //     <div class="row mb-0">
+  //       <label class="cell-sm-6">Probe Feedrate</label>
+  //       <div class="cell-sm-6">
+  //           <input id="centerProbeFeedrate" type="number" value="100" data-role="input" data-append="mm/min" data-prepend="<i class='fas fa-running'></i>" data-clear-button="false">
+  //       </div>
+  //     </div>
+  //     <small>How fast the probe will move - slower is safer/more accurate</small>
+  //     `,
+  //   actions: [{
+  //       caption: "Run center finding Probe",
+  //       cls: "js-dialog-close success",
+  //       onclick: function() {
+  //         var approxCircleDia = parseFloat($("#centerProbeDistance").val())
+  //         var endmillDiameter = parseFloat($("#centerProbeEndmill").val())
+  //         var probeFeed = parseInt($("#centerProbeFeedrate").val())
+  //         findCircleCenter(approxCircleDia, endmillDiameter, probeFeed)
+  //       }
+  //     },
+  //     {
+  //       caption: "Cancel",
+  //       cls: "js-dialog-close alert",
+  //       onclick: function() {
+  //         //
+  //       }
+  //     }
+  //   ]
+  // });
